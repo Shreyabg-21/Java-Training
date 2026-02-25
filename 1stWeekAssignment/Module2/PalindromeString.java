a7f506c1fa5642e5b90c92d38a531cac3d25e019
Shreyabg-21 <sshreyabg@gmail.com>
2026-02-25 16:42:17 +0530

    Add files via upload

---
+++ 1stWeekAssignment/Module2/PalindromeString.java
@@ -0,0 +1,22 @@
+package Javaprogram;
+import java.util.Scanner;
+
+public interface PalindromeString {
+	    static boolean isPalindrome(String str) {
+	        String rev = "";
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            rev = rev + str.charAt(i);
+	        }
+	        return str.equals(rev);
+	    }
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String str = sc.nextLine();
+	        if (isPalindrome(str))
+	            System.out.println("Palindrome");
+	        else
+	            System.out.println("Not a Palindrome");
+	        sc.close();
+	    }
+	}
